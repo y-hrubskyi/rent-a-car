24a61b08499d3856a399c8519b01c5ba5df614e5
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-06 16:25:52 +0200

    feat: init route-library and configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import { SharedLayout } from "@/components/SharedLayout/SharedLayout";
+import HomePage from "@/pages/HomePage/HomePage";
+import CatalogPage from "@/pages/CatalogPage/CatalogPage";
+import FavoritesPage from "@/pages/FavoritesPage/FavoritesPage";
+
 export const App = () => {
-  return <h1 style={{ textAlign: "center" }}>Rent-a-car App</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,3 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export const SharedLayout = () => {
-  return <div style={{ textAlign: "center" }}>SharedLayout</div>;
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>SharedLayout</div>
+      <Outlet />
+    </>
+  );
 };
