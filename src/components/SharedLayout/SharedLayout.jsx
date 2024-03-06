fa1695dd4c67d32177dd35b8b82a39fa85cd0efa
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-06 16:37:55 +0200

    frat: add basic navigation

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,14 @@
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const SharedLayout = () => {
   return (
     <>
-      <div style={{ textAlign: "center" }}>SharedLayout</div>
+      <div style={{ display: "flex", justifyContent: "center", gap: 20 }}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/catalog">Catalog</NavLink>
+        <NavLink to="/favorites"> Favorites</NavLink>
+      </div>
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
