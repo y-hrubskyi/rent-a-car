408b991354134cfd9273f932646e1907123cca19
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-07 19:08:59 +0200

    feat: create header

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  padding-top: ${(p) => p.theme.spacing(10)};
+  padding-bottom: ${(p) => p.theme.spacing(10)};
+`;
