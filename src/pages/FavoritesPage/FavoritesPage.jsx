2b64d15441d021a2d712ceceb16c3173a71571ca
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-09 08:39:21 +0200

    feat: update FavoritesPage
     - get favorites from store (localStorage)
     - render AdvertsList from favorites

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from "react-redux";
+
+import { AdvertsList } from "@/components/AdvertsList/AdvertsList";
+import { selectFavoritesAdverts } from "@/store/adverts/selectors";
+
 const FavoritesPage = () => {
-  return <h2 style={{ textAlign: "center" }}>Favorites Page</h2>;
+  const data = useSelector(selectFavoritesAdverts);
+
+  return data && <AdvertsList adverts={data} />;
 };
 
 export default FavoritesPage;
