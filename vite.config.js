a850dd8a5408b088ebc807a45ca24df5dcac8aa5
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-09 07:42:27 +0200

    feat: install axios library, create instance, upd aliases

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       "@/assets": "/src/assets",
       "@/components": "/src/components",
       "@/pages": "/src/pages",
+      "@/services": "/src/services",
       "@/styles": "/src/styles",
     },
   },
