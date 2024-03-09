346bed2c2401d410d67c3a813ece30ee1b7bfa2e
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-09 08:04:31 +0200

    feat: add fetchAdverts in CatalogPage

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchAdverts } from "@/store/adverts/operations";
+import { selectAdverts } from "@/store/adverts/selectors";
+
 const CatalogPage = () => {
+  const dispatch = useDispatch();
+  const data = useSelector(selectAdverts);
+
+  useEffect(() => {
+    dispatch(fetchAdverts());
+  }, [dispatch]);
+
+  console.log(data);
+
   return <h2 style={{ textAlign: "center" }}>Catalog Page</h2>;
 };
 
