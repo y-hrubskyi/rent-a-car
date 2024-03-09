413107e0974602321917210e7d54da8e3ecc1ea5
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-09 08:41:39 +0200

    feat: add Placeholder to FavoritesPage

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,12 +1,17 @@
 import { useSelector } from "react-redux";
 
+import { Placeholder } from "@/components/Placeholder/Placeholder";
 import { AdvertsList } from "@/components/AdvertsList/AdvertsList";
 import { selectFavoritesAdverts } from "@/store/adverts/selectors";
 
 const FavoritesPage = () => {
   const data = useSelector(selectFavoritesAdverts);
 
-  return data && <AdvertsList adverts={data} />;
+  if (data.length === 0) {
+    return <Placeholder>Your list of favorites is currently empty</Placeholder>;
+  }
+
+  return <AdvertsList adverts={data} />;
 };
 
 export default FavoritesPage;
