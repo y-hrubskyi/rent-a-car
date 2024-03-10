793eb1c43b30d828a0a611c6ae0af18520b5170e
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-10 17:27:34 +0200

    feat: create feature-section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import { HeroSection } from "./HeroSection/HeroSection";
+import { FeatureSection } from "./FeatureSection/FeatureSection";
 import { PageTitle } from "./HomePage.styled";
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
     <>
       <PageTitle>Welcome to our car agency</PageTitle>
       <HeroSection />
+      <FeatureSection />
     </>
   );
 };
